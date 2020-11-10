ef9c420227d915a2fa950815e72e6149fb4c4a2d
Nikhil Racha <rachanikhil41@gmail.com>
2020-11-10 08:11:32 -0500

    Update Input.js

--- src/views/Helpdesk/Contact/components/controls/Input.js
+++ src/views/Helpdesk/Contact/components/controls/Input.js
@@ -6,6 +6,7 @@ export default function Input(props) {
     const { name, label, value,error=null, onChange } = props;
     return (
         <TextField
+            variant = {"outlined"}
             label={label}
             name={name}
             value={value}
